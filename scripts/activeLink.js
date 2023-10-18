19fe7b5b17fa789420e23ad901da2e7aa7fbdd12
ze-e <white.noise616@gmail.com>
2023-10-18 17:06:08 -0600

    fix active link

--- scripts/activeLink.js
+++ scripts/activeLink.js
@@ -1,18 +1,27 @@
+// Function to add the "active" class to the corresponding nav-link
+function setActiveNavLinks() {
+  const sections = document.querySelectorAll(".section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  // Add an event listener for scrolling
+  window.addEventListener("scroll", () => {
+    const fromTop = window.scrollY;
+
+    navLinks.forEach((navLink, index) => {
+      const section = sections[index];
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        navLink.classList.add("active");
+      } else {
+        navLink.classList.remove("active");
+      }
+    });
+  });
+}
+
+// Call the function when the page loads
 document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".nav-link");
-  const sections = document.querySelectorAll("section");
-
-  function changeLinkState() {
-    let index = sections.length;
-
-    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
-    links.forEach((link) => link.classList.remove("active"));
-    links[index].classList.add("active");
-  }
-
-  changeLinkState();
-  window.addEventListener("scroll", changeLinkState);
+  setActiveNavLinks();
 });
-
-// https://stackoverflow.com/questions/52025615/vanilla-js-change-active-state-of-links-when-scrolling-refactoring
